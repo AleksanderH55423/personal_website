9ae94ae73aa5e1165c5ba907420100f7c55d8a9c
HovlaOsk <158601761+HovlaOsk@users.noreply.github.com>
2025-12-05 14:21:44 -0500

    added correct contact options

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,8 @@ export default function ContactPage() {
     <Base>
       <div className="section container text-center">
         <h1 className="text-5xl font-bold mb-6">Contact</h1>
-        <p className="text-xl">Email: aleksander.hovland@gatech.edu</p>
+        <p className="text-xl">Email: aleksander.hovland@gmail.com</p>
+        <p className="text-xl">text me: +1 (936)-777-1228</p>
       </div>
     </Base>
   );
