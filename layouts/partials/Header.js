34d1fb60c54d71aacab014e6c068d4f0dda0cf2e
HovlaOsk <158601761+HovlaOsk@users.noreply.github.com>
2025-12-05 00:20:23 -0500

    Fixed header categories bug

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -1,47 +1,31 @@
 import menu from "@config/menu.json";
-import { useHeaderContext } from "context/state";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+
 const Header = () => {
-  // router
   const router = useRouter();
 
-  //context
-  const { categories } = useHeaderContext();
-
-  //local state
+  // local state
   const [openMenu, setOpenMenu] = useState(false);
+
+  // We no longer have categories, so navMenu is just the static menu
   const [navMenu, setNavMenu] = useState(
     menu.main.map((item) => ({ ...item, type: "main" }))
   );
 
   useEffect(() => {
-    const matchRoute = menu.main.find((item) => item.url === router.asPath);
-    const navList = [...menu.main];
-
-    if (matchRoute) {
-      if (matchRoute.url === "/") {
-        const arr = categories.slice(0, 4);
-        navList.splice(1, 0, ...arr);
-        setNavMenu(navList);
-      } else {
-        setNavMenu(menu.main);
-      }
-    } else {
-      //if route not match
-      navList.splice(1, 0, ...categories);
-      navList.splice(navList.length - 2, menu.main.length - 1);
-      setNavMenu(navList);
-    }
+    // Since categories no longer exist, we always use menu.main
+    setNavMenu(menu.main);
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [router.asPath, menu.main, categories]);
+  }, [router.asPath]);
 
   return (
     <>
       <header className={`header mt-8 pt-12 pb-3`}>
         <nav className="navbar container text-center md:text-left">
+
           {/* navbar toggler */}
           <button
             className="btn btn-primary inline-flex items-center md:hidden"
@@ -63,12 +47,12 @@ const Header = () => {
             )}
             Menu
           </button>
-          {/* /navbar toggler */}
 
+          {/* Back to home when not on homepage */}
           {router.asPath !== "/" && (
             <div className="hidden text-center md:block">
               <Link
-                className="mb-12  inline-flex items-center text-primary hover:underline"
+                className="mb-12 inline-flex items-center text-primary hover:underline"
                 href="/"
               >
                 <svg
@@ -89,29 +73,27 @@ const Header = () => {
             </div>
           )}
 
+          {/* navigation */}
           <ul
             id="nav-menu"
             className={`navbar-nav order-3 ${
               !openMenu && "hidden"
             } w-full justify-center md:flex md:w-auto md:space-x-2 md:order-1`}
           >
-            {navMenu.map((menu, i) => (
+            {navMenu.map((menuItem, i) => (
               <React.Fragment key={`menu-${i}`}>
-                {menu.hasChildren ? (
+                {menuItem.hasChildren ? (
                   <li className="nav-item nav-dropdown group relative">
                     <span className="nav-link inline-flex items-center">
-                      {menu.name}
+                      {menuItem.name}
                       <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
                         <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                       </svg>
                     </span>
                     <ul className="nav-dropdown-list hidden group-hover:block md:invisible md:absolute md:block md:opacity-0 md:group-hover:visible md:group-hover:opacity-100">
-                      {menu.children.map((child, i) => (
+                      {menuItem.children.map((child, i) => (
                         <li className="nav-dropdown-item" key={`children-${i}`}>
-                          <Link
-                            href={child.url}
-                            className="nav-dropdown-link block"
-                          >
+                          <Link href={child.url} className="nav-dropdown-link block">
                             {child.name}
                           </Link>
                         </li>
@@ -122,12 +104,12 @@ const Header = () => {
                   <li className="nav-item">
                     <Link
                       onClick={() => setOpenMenu(false)}
-                      href={menu.url}
+                      href={menuItem.url}
                       className={`nav-link inline-block ${
-                        !menu.url.includes("/categories") && "text-dark"
-                      } ${router.asPath === menu.url && "nav-link-active"}`}
+                        router.asPath === menuItem.url && "nav-link-active"
+                      }`}
                     >
-                      {menu.name}
+                      {menuItem.name}
                     </Link>
                   </li>
                 )}
