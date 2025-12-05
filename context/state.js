b6ffc40c60a5bd24966c48df3f7924a0460b6dc1
HovlaOsk <158601761+HovlaOsk@users.noreply.github.com>
2025-12-04 23:48:06 -0500

    Removed blog category from context

--- context/state.js
+++ context/state.js
@@ -1,19 +1,13 @@
 import { createContext, useContext } from "react";
-import posts from "../.json/posts.json";
 
-const HeaderContext = createContext();
+const HeaderContext = createContext({});
+
 export const TaxonomySlugProvider = ({ children }) => {
-  const categories = [
-    ...new Set(
-      posts.reduce((acc, post) => [...acc, ...post.frontmatter.categories], [])
-    ),
-  ].map((item) => ({ name: item, url: `/categories/${item.toLowerCase()}` }));
   return (
-    <HeaderContext.Provider value={{ categories }}>
+    <HeaderContext.Provider value={{}}>
       {children}
     </HeaderContext.Provider>
   );
 };
-export const useHeaderContext = () => {
-  return useContext(HeaderContext);
-};
+
+export const useHeaderContext = () => useContext(HeaderContext);
